7c23e179849812fc5a38ddd10efec5552f62c25c
Brave Link <171126727+mkyts98@users.noreply.github.com>
2024-07-03 13:24:36 +0200

    Reinitializing Commune to resolve node disconnections

--- smartdrive/commune/utils.py
+++ smartdrive/commune/utils.py
@@ -3,33 +3,23 @@ import asyncio
 from communex._common import ComxSettings
 from communex.client import CommuneClient
 
+DEFAULT_NUM_CONNECTIONS = 1
 
-async def _try_get_client(url, num_connections):
+
+def _try_get_client(url, num_connections):
     try:
         return CommuneClient(url, num_connections=num_connections)
     except Exception:
         return None
 
 
-async def get_comx_client(num_connections, testnet) -> CommuneClient:
+def get_comx_client(testnet: bool, num_connections: int = DEFAULT_NUM_CONNECTIONS) -> CommuneClient:
     comx_settings = ComxSettings()
     urls = comx_settings.TESTNET_NODE_URLS if testnet else comx_settings.NODE_URLS
 
-    tasks = [asyncio.create_task(_try_get_client(url, num_connections)) for url in urls]
-
-    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-
-    first_valid_client = None
-    for task in done:
-        comx_client = task.result()
+    for url in urls:
+        comx_client = _try_get_client(url, num_connections)
         if comx_client is not None:
-            first_valid_client = comx_client
-            break
-
-    for task in pending:
-        task.cancel()
-
-    if first_valid_client:
-        return first_valid_client
+            return comx_client
 
     raise Exception("No valid comx_client could be found")
