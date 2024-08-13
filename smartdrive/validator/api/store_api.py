1cf266907ec20cd58d6a0f99fee1d1221ef07dc4
Brave Link <171126727+mkyts98@users.noreply.github.com>
2024-08-13 13:23:49 +0200

    Delete the chunks of the miners that have saved the files if an exception has been generated in store new file

--- smartdrive/validator/api/store_api.py
+++ smartdrive/validator/api/store_api.py
@@ -24,7 +24,7 @@ import asyncio
 import random
 import time
 import uuid
-from typing import Optional
+from typing import Optional, List, Tuple
 
 from substrateinterface import Keypair
 from fastapi import Form, UploadFile, HTTPException, Request
@@ -35,6 +35,7 @@ from communex.types import Ss58Address
 from smartdrive.commune.errors import CommuneNetworkUnreachable
 from smartdrive.validator.api.middleware.sign import sign_data
 from smartdrive.validator.api.middleware.subnet_middleware import get_ss58_address_from_public_key
+from smartdrive.validator.api.utils import remove_chunk_request
 from smartdrive.validator.config import config_manager
 from smartdrive.validator.database.database import Database
 from smartdrive.models.event import MinerProcess, StoreEvent, StoreParams, StoreInputParams, ChunkEvent
@@ -136,6 +137,7 @@ async def store_new_file(
     final_chunks: list[ChunkEvent] = []
     miners_processes = []
     stored_chunks = []
+    stored_miners: List[Tuple[ModuleInfo, str]] = []
 
     async def handle_store_request(miner: ModuleInfo, chunk_data: bytes, chunk_index: int) -> bool:
         start_time = time.time()
@@ -158,6 +160,7 @@ async def store_new_file(
 
         if succeed:
             stored_chunks.append((chunk_uuid, chunk_index, chunk_data))
+            stored_miners.append((miner, chunk_uuid))
 
         return succeed
 
@@ -181,59 +184,78 @@ async def store_new_file(
         if replication_count < MIN_REPLICATION_FOR_FILE and not available_miners:
             raise HTTPException(status_code=500, detail=f"Failed to store chunk {chunk_index} in the required minimum number of miners.")
 
-    if validating:
-        await asyncio.gather(*[handle_store_request(miner, file_bytes, 0) for miner in miners])
-        if not stored_chunks:
-            return None
-    else:
-        num_chunks = min(len(miners), MAX_MINERS_FOR_FILE)
-        chunk_size = max(1, len(file_bytes) // num_chunks)
-        remainder = len(file_bytes) % num_chunks
-        chunks = [file_bytes[i * chunk_size:(i + 1) * chunk_size] for i in range(num_chunks)]
+    async def remove_stored_chunks():
+        if stored_miners:
+            remove_tasks = [
+                asyncio.create_task(
+                    remove_chunk_request(
+                        keypair=validator_keypair,
+                        user_ss58_address=user_ss58_address,
+                        miner=miner,
+                        chunk_uuid=chunk_uuid
+                    )
+                )
+                for miner, chunk_uuid in stored_miners
+            ]
+            await asyncio.gather(*remove_tasks)
 
-        if remainder:
-            chunks[-1] += file_bytes[-remainder:]
+    try:
+        if validating:
+            await asyncio.gather(*[handle_store_request(miner, file_bytes, 0) for miner in miners])
+            if not stored_chunks:
+                return None
+        else:
+            num_chunks = min(len(miners), MAX_MINERS_FOR_FILE)
+            chunk_size = max(1, len(file_bytes) // num_chunks)
+            remainder = len(file_bytes) % num_chunks
+            chunks = [file_bytes[i * chunk_size:(i + 1) * chunk_size] for i in range(num_chunks)]
 
-        store_tasks = [store_chunk_with_redundancy(chunk, index) for index, chunk in enumerate(chunks)]
-        await asyncio.gather(*store_tasks)
+            if remainder:
+                chunks[-1] += file_bytes[-remainder:]
 
-        if len(stored_chunks) != len(chunks) * MIN_REPLICATION_FOR_FILE:
-            raise HTTPException(status_code=500, detail="Failed to store all chunks in the required number of miners.")
+            store_tasks = [store_chunk_with_redundancy(chunk, index) for index, chunk in enumerate(chunks)]
+            await asyncio.gather(*store_tasks)
 
-    for chunk_uuid, chunk_index, file in stored_chunks:
-        sub_chunk_start = random.randint(0, max(0, len(file) - MAX_ENCODED_RANGE))
-        sub_chunk_end = min(sub_chunk_start + MAX_ENCODED_RANGE, len(file))
-        sub_chunk_encoded = file[sub_chunk_start:sub_chunk_end].hex()
+            if len(stored_chunks) != len(chunks) * MIN_REPLICATION_FOR_FILE:
+                raise HTTPException(status_code=500, detail="Failed to store all chunks in the required number of miners.")
 
-        chunk = ChunkEvent(
-            uuid=chunk_uuid,
-            chunk_index=chunk_index,
-            sub_chunk_start=sub_chunk_start,
-            sub_chunk_end=sub_chunk_end,
-            sub_chunk_encoded=sub_chunk_encoded
+        for chunk_uuid, chunk_index, file in stored_chunks:
+            sub_chunk_start = random.randint(0, max(0, len(file) - MAX_ENCODED_RANGE))
+            sub_chunk_end = min(sub_chunk_start + MAX_ENCODED_RANGE, len(file))
+            sub_chunk_encoded = file[sub_chunk_start:sub_chunk_end].hex()
+
+            chunk = ChunkEvent(
+                uuid=chunk_uuid,
+                chunk_index=chunk_index,
+                sub_chunk_start=sub_chunk_start,
+                sub_chunk_end=sub_chunk_end,
+                sub_chunk_encoded=sub_chunk_encoded
+            )
+            final_chunks.append(chunk)
+
+        event_params = StoreParams(
+            file_uuid=f"{int(time.time())}_{str(uuid.uuid4())}",
+            miners_processes=miners_processes,
+            created_at=int(time.time() * 1000) if validating else None,
+            expiration_ms=get_file_expiration() if validating else None,
+            chunks=final_chunks
         )
-        final_chunks.append(chunk)
 
-    event_params = StoreParams(
-        file_uuid=f"{int(time.time())}_{str(uuid.uuid4())}",
-        miners_processes=miners_processes,
-        created_at=int(time.time() * 1000) if validating else None,
-        expiration_ms=get_file_expiration() if validating else None,
-        chunks=final_chunks
-    )
+        signed_params = sign_data(event_params.dict(), validator_keypair)
 
-    signed_params = sign_data(event_params.dict(), validator_keypair)
+        return StoreEvent(
+            uuid=f"{int(time.time())}_{str(uuid.uuid4())}",
+            validator_ss58_address=Ss58Address(validator_keypair.ss58_address),
+            event_params=event_params,
+            event_signed_params=signed_params.hex(),
+            user_ss58_address=user_ss58_address,
+            input_params=StoreInputParams(file=calculate_hash(file_bytes)),
+            input_signed_params=input_signed_params
+        )
 
-    return StoreEvent(
-        uuid=f"{int(time.time())}_{str(uuid.uuid4())}",
-        validator_ss58_address=Ss58Address(validator_keypair.ss58_address),
-        event_params=event_params,
-        event_signed_params=signed_params.hex(),
-        user_ss58_address=user_ss58_address,
-        input_params=StoreInputParams(file=calculate_hash(file_bytes)),
-        input_signed_params=input_signed_params
-    )
-
+    except Exception:
+        await remove_stored_chunks()
+        raise
 
 async def _store_request(keypair: Keypair, miner: ModuleInfo, user_ss58_address: Ss58Address, file_bytes: bytes) -> Optional[MinerWithChunk]:
     """
